0500839ca9b3276e9d4f5f2c6a492d769f4be61b
Arnas Radzevicius <arnas@LT-LIT-SC-0132.internal.visma.com>
2020-02-03 15:05:59 +0200

    initial commit

---
+++ SpringBootTask/src/test/java/com/arnasrad/SpringBootTask/SpringBootTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arnasrad.SpringBootTask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
