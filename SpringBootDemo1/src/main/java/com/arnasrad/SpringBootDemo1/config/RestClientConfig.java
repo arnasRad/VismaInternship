1cc057bf13377c1c41fb8034ca9d3a4866fa0b05
arnasRad <arnasrad@gmail.com>
2020-02-06 20:03:13 +0200

    RestClientConfig implemented; should refresh the token if http response is unauthorized; NOT TESTED

--- SpringBootDemo1/src/main/java/com/arnasrad/SpringBootDemo1/config/RestClientConfig.java
+++ SpringBootDemo1/src/main/java/com/arnasrad/SpringBootDemo1/config/RestClientConfig.java
@@ -1,5 +1,6 @@
 package com.arnasrad.SpringBootDemo1.config;
 
+import com.arnasrad.SpringBootDemo1.utils.RestTemplateHeaderModifierInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.ClientHttpRequestInterceptor;
@@ -8,7 +9,7 @@ import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
 import java.util.List;
-/*
+
 @Configuration
 public class RestClientConfig {
 
@@ -20,8 +21,8 @@ public class RestClientConfig {
         if (CollectionUtils.isEmpty(interceptors)) {
             interceptors = new ArrayList<>();
         }
-        interceptors.add(new Resttemplateheader)
-
+        interceptors.add(new RestTemplateHeaderModifierInterceptor());
+        restTemplate.setInterceptors(interceptors);
+        return restTemplate;
     }
 }
-*/
