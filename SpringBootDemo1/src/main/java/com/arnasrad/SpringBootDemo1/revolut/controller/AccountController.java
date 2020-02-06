9e7636224283c2413ba387bdef838bdf4ecddbca
arnasRad <arnasrad@gmail.com>
2020-02-06 17:54:53 +0200

    post revert commit

--- SpringBootDemo1/src/main/java/com/arnasrad/SpringBootDemo1/revolut/controller/AccountController.java
+++ SpringBootDemo1/src/main/java/com/arnasrad/SpringBootDemo1/revolut/controller/AccountController.java
@@ -1,15 +1,17 @@
 package com.arnasrad.SpringBootDemo1.revolut.controller;
 
 import com.arnasrad.SpringBootDemo1.revolut.model.Account;
+import com.arnasrad.SpringBootDemo1.revolut.service.JwtTokenService;
+import com.arnasrad.SpringBootDemo1.revolut.service.KeyLoadService;
+import com.arnasrad.SpringBootDemo1.revolut.service.RefreshTokenService;
 import com.arnasrad.SpringBootDemo1.revolut.util.Utils;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,12 +22,16 @@ import java.util.List;
 @RestController
 @RequestMapping
 public class AccountController {
+    @Value("${revolut.sandbox.constant.accessToken}")
+    private String accessToken;
+
+    @Value("${url.revolut.accounts}")
+    private String accountsUrl;
+
+    @Autowired
+    RefreshTokenService refreshTokenService;
 
     RestTemplate restTemplate = new RestTemplate();
-    final String revolutUrl = "https://sandbox-b2b.revolut.com/api/1.0";
-
-    @Value("${revolut.sandbox.accessToken.constant}")
-    private String accessToken;
 
     @GetMapping("/")
     String mainPage() {
@@ -33,21 +39,39 @@ public class AccountController {
     }
 
     @GetMapping("/revolut")
-    List<Account> getAccounts() throws JsonProcessingException {
+    List<Account> getAccounts() {
         HttpHeaders httpHeaders = new HttpHeaders();
         // TODO: implement refresh token
         httpHeaders.setBearerAuth(accessToken);
         httpHeaders.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
 
-        MultiValueMap<String, String> map = new LinkedMultiValueMap<>();
-        map.add("", "");
-
         HttpEntity<String> httpEntity = new HttpEntity<>(httpHeaders);
 
-        String jsonResponse = restTemplate.exchange(revolutUrl + "/accounts", HttpMethod.GET, httpEntity, String.class).getBody();
-        if (jsonResponse != null)
-            return Utils.getAccountListFromJsonString(jsonResponse);
-        else
+        try {
+            String jsonResponse = restTemplate.exchange(accountsUrl, HttpMethod.GET, httpEntity, String.class).getBody();
+            if (jsonResponse != null)
+                return Utils.getAccountListFromJsonString(jsonResponse);
+            else
+                return null;
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
             return null;
+        }
+    }
+
+    @Autowired
+    JwtTokenService jwtTokenService;
+    @Autowired
+    KeyLoadService keyLoadService;
+    @GetMapping("/refresh-token")
+    String getJwtToken() {
+        try {
+            String result = refreshTokenService.refreshToken();
+            return result;
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+            return null;
+        }
+        //return jwtTokenService.generateToken();
     }
 }
